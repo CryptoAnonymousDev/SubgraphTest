969cc8fda57303b8e3e300e0f4899a7014d0b169
Sebastian Bauer <sebastian@synapse.network>
2022-07-30 00:17:06 +0200

    First version of subgraph

--- src/the-graph-contract.ts
+++ src/the-graph-contract.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, Bytes } from "@graphprotocol/graph-ts"
 import {
   TheGraphContract,
   CarCreated,
@@ -11,52 +11,14 @@ import {
   FundsDeposited,
   FundsWithdrawn
 } from "../generated/TheGraphContract/TheGraphContract"
-import { ExampleEntity } from "../generated/schema"
+import { Client, Car } from "../generated/schema"
 
 export function handleCarCreated(event: CarCreated): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.carId = event.params.carId
-  entity.brand = event.params.brand
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.carNonce(...)
-  // - contract.carOwnership(...)
-  // - contract.cars(...)
-  // - contract.clientNonce(...)
-  // - contract.clients(...)
+  let car = new Car(Bytes.fromHexString(event.params.carId.toHexString()))
+  car.brand = event.params.brand
+  car.model = event.params.model
+  car.price = event.params.price
+  car.save()
 }
 
 export function handleCarSold(event: CarSold): void {}
